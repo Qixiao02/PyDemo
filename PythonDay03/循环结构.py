e483da0ea858b8cae8107ffe6dc0adbddad29320
qixiao <porlong@qq.com>
2022-06-10 15:15:28 +0800

    求1-100奇数、偶数之和

--- PythonDay03/循环结构.py
+++ PythonDay03/循环结构.py
@@ -1,5 +1,11 @@
 #for in循环
+#求1-100偶数之和
 num = 0
-for num in range(1,101):
-    num % 2 == 0
-    print(num)
+for x in range(2, 101, 2): #(开区间,闭区间,步长)
+    num += x
+print(num)
+#求1-100奇数之和
+num2 = 0
+for y in range(1, 101, 2):
+    num2 +=y
+print(num2)
