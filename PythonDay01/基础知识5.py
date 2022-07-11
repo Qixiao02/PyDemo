ea1c024358be8b2082a5d92ed98d0f9f073296a0
qixiao <porlong@qq.com>
2022-07-11 13:23:41 +0800

    字典与列表深入学习

--- PythonDay01/基础知识5.py
+++ PythonDay01/基础知识5.py
@@ -33,8 +33,9 @@ print(alien_0)
 """
 favorite_language = {
     'twy': 'python',
-    'cby': 'C',
-    'hzh': 'html',
+    'cby': 'c',
+    'hzh': 'java',
+    'hlh': 'c++',
 }
 language =favorite_language['twy'].title()
 print(f"twy最喜欢的语言是:{language}")
@@ -50,3 +51,73 @@ print(point_value)
 alien_3 = {'color': 'green', 'speed': 'slow', }
 point_value = alien_3.get('points', "这个键没有值")
 print(point_value)
+"""遍历键值对"""
+user_0 = {
+    'username': 'zyj',
+    'frist': 'enrico',
+    'last': 'fermi',
+}
+for key, value in user_0.items():
+    print(f"\nkey:{key}")
+    print(f"Value:{value}")
+print("\n")
+"""遍历字典中所有键与值"""
+for name in favorite_language.keys():
+    print(name.title())
+print("\n")
+for value in favorite_language.values():
+    print(value.title())
+print("\n")
+"""按照特定顺序遍历字典中的所有键"""
+for name in sorted(favorite_language.keys()):
+    print(f"{name.title()},认为你参加了投票")
+"""按照特定顺序遍历字典中的所有值"""
+print("\n")
+for value in sorted(favorite_language.values()):
+    print(f"{value.title()},是一种编程语言")
+print("\n")
+"""set()方法找出列表中独一无二的元素，并用这些元素来创建一个集合"""
+for language in set(favorite_language.values()):
+    print(language.title())
+print("\n")
+languages = {'python', 'c', 'java'}
+print(languages)
+"""练习项目"""
+river = {
+    'nile': 'egypt',
+    'yellow river': 'china',
+    'mississippi river': 'America'
+}
+"""6-5"""
+for key, value in river.items():
+    print(f"{key.title()},流经{value.title()}")
+for river, nation in river.items():
+    print(f"键:{river.title()}")
+    print(f"值:{nation.title()}")
+print("\n")
+"""6.4嵌套"""
+#创建30个绿色外星人
+aliens = []
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+#显示前五
+for alien in aliens[:5]:
+    print(alien)
+#查看一共有多少个外星人
+print(f"一共有{len(aliens)}个外星人")
+print("\n")
+"""使用for循环与if,elif修改外星人信息"""
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+for alien in aliens[:5]:
+    print(f"新的外星人信息:{alien}")
+"""在字典中存储列表"""
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese']
+}
+print(f'你点了一个{pizza["crust"]}- crust pizza')
