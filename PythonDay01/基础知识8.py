22136ce5330568a0f45cfd2e00bb41c43cf8bfa9
qixiao <porlong@qq.com>
2022-08-21 01:11:18 +0800

    父类继承

--- PythonDay01/基础知识8.py
+++ PythonDay01/基础知识8.py
@@ -27,9 +27,10 @@ class Car:
         self.factory = factory
         self.day = day
         self.carmodel = carmodel
+        self.fill_gas_tank = 35
         self.car_kilometers = 0
     def get_car_info(self):
-        car_info = f"车商:{self.factory} 车龄:{self.day}天 车型:{self.carmodel} 已行驶里程数:{self.car_kilometers}KM"
+        car_info = f"车商:{self.factory} 车龄:{self.day}天 车型:{self.carmodel} 油箱容量:{self.fill_gas_tank}L 已行驶里程数:{self.car_kilometers}KM"
         return car_info.title()
 
     def auto_add_car_kilometers(self, car_kilometers):
@@ -42,9 +43,9 @@ class Car:
         else:
             self.car_kilometers = new_car_kilometers
 
-    def show(self):
-        swap = self.car_kilometers
-        print(swap)
+    def car_fill_gas_tank(self, fill_gas_tanks):
+        self.fill_gas_tank = fill_gas_tanks
+
 my_car = Car('bwm', '756', 'Small Family Car')
 #给属性指定默认值
 # print(my_car.get_car_kilometers())
@@ -53,6 +54,7 @@ my_car = Car('bwm', '756', 'Small Family Car')
 # my_car.car_kilometers = 312
 #通过方法修改属性值
 my_car.updata_car_kilometers(43679)
+my_car.car_fill_gas_tank(35)
 # my_car.updata_car_kilometers(-2)
 # my_car.auto_add_car_kilometers(200)
 print(my_car.get_car_info())
@@ -61,6 +63,7 @@ print(my_car.get_car_info())
 my_new_car = Car('RollsRoyce', '3', 'Small Family Car')
 my_new_car.updata_car_kilometers(8)
 my_new_car.auto_add_car_kilometers(6)
+my_new_car.car_fill_gas_tank(40)
 print(my_new_car.get_car_info())
 
 "类继承"
@@ -68,7 +71,15 @@ class Electriccar(Car):
     def __init__(self, factory, day, carmodel):
         #初始化父类的属性
         super().__init__(factory, day, carmodel)
-my_electric_car = Electriccar('tesla', '247' ,'Small Family Car')
+        self.battery_size = 75
+    def electric_car_battery(self):
+        print(f"这辆电车的电池容量还剩余:{self.battery_size}kwh")
+    def car_fill_gas_tank(self, fill_gas_tanks):
+        fill_gas_tanks = f'这是辆电车！'
+my_electric_car = Electriccar('tesla', '247','Small Family Car')
 my_electric_car.updata_car_kilometers(16352)
 my_electric_car.auto_add_car_kilometers(13)
+my_electric_car.car_fill_gas_tank(38)
 print(my_electric_car.get_car_info())
+my_electric_car.electric_car_battery()
+
