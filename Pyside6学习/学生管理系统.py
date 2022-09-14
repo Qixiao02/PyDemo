509f2e83ea626ab41105ac8f2fa6bcc88f8d0f71
qixiao <porlong@qq.com>
2022-09-14 17:02:23 +0800

    test

--- Pyside6学习/学生管理系统.py
+++ Pyside6学习/学生管理系统.py
@@ -15,20 +15,109 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QHeaderView, QMainWindow,
-    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
-    QTableView, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QLabel,
+    QLineEdit, QMainWindow, QMenuBar, QPushButton,
+    QSizePolicy, QStatusBar, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(803, 624)
+        MainWindow.resize(982, 624)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.layoutWidget = QWidget(self.centralwidget)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(10, 10, 711, 227))
+        self.gridLayout = QGridLayout(self.layoutWidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.lineEdit_4 = QLineEdit(self.layoutWidget)
+        self.lineEdit_4.setObjectName(u"lineEdit_4")
+
+        self.gridLayout.addWidget(self.lineEdit_4, 1, 6, 1, 1)
+
+        self.label_3 = QLabel(self.layoutWidget)
+        self.label_3.setObjectName(u"label_3")
+
+        self.gridLayout.addWidget(self.label_3, 0, 5, 1, 1)
+
+        self.lineEdit_7 = QLineEdit(self.layoutWidget)
+        self.lineEdit_7.setObjectName(u"lineEdit_7")
+
+        self.gridLayout.addWidget(self.lineEdit_7, 2, 1, 1, 1)
+
+        self.label = QLabel(self.layoutWidget)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
+        self.lineEdit = QLineEdit(self.layoutWidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.gridLayout.addWidget(self.lineEdit, 0, 1, 1, 1)
+
+        self.label_7 = QLabel(self.layoutWidget)
+        self.label_7.setObjectName(u"label_7")
+
+        self.gridLayout.addWidget(self.label_7, 2, 0, 1, 1)
+
+        self.label_8 = QLabel(self.layoutWidget)
+        self.label_8.setObjectName(u"label_8")
+
+        self.gridLayout.addWidget(self.label_8, 5, 0, 1, 1)
+
+        self.label_2 = QLabel(self.layoutWidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout.addWidget(self.label_2, 0, 2, 1, 1)
+
+        self.comboBox = QComboBox(self.layoutWidget)
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setMinimumSize(QSize(193, 0))
+
+        self.gridLayout.addWidget(self.comboBox, 0, 4, 1, 1)
+
+        self.label_5 = QLabel(self.layoutWidget)
+        self.label_5.setObjectName(u"label_5")
+
+        self.gridLayout.addWidget(self.label_5, 1, 2, 1, 1)
+
+        self.label_6 = QLabel(self.layoutWidget)
+        self.label_6.setObjectName(u"label_6")
+
+        self.gridLayout.addWidget(self.label_6, 1, 5, 1, 1)
+
+        self.lineEdit_5 = QLineEdit(self.layoutWidget)
+        self.lineEdit_5.setObjectName(u"lineEdit_5")
+
+        self.gridLayout.addWidget(self.lineEdit_5, 1, 1, 1, 1)
+
+        self.lineEdit_8 = QLineEdit(self.layoutWidget)
+        self.lineEdit_8.setObjectName(u"lineEdit_8")
+
+        self.gridLayout.addWidget(self.lineEdit_8, 5, 1, 1, 4)
+
+        self.lineEdit_6 = QLineEdit(self.layoutWidget)
+        self.lineEdit_6.setObjectName(u"lineEdit_6")
+
+        self.gridLayout.addWidget(self.lineEdit_6, 1, 4, 1, 1)
+
+        self.lineEdit_3 = QLineEdit(self.layoutWidget)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+
+        self.gridLayout.addWidget(self.lineEdit_3, 0, 6, 1, 1)
+
+        self.label_4 = QLabel(self.layoutWidget)
+        self.label_4.setObjectName(u"label_4")
+
+        self.gridLayout.addWidget(self.label_4, 1, 0, 1, 1)
+
         self.verticalLayoutWidget = QWidget(self.centralwidget)
         self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(710, 0, 91, 131))
+        self.verticalLayoutWidget.setGeometry(QRect(780, 10, 192, 231))
         self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
@@ -48,29 +137,15 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addWidget(self.pushButton_3)
 
-        self.horizontalLayoutWidget = QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
-        self.horizontalLayoutWidget.setGeometry(QRect(0, 200, 801, 381))
-        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.tableView = QTableView(self.horizontalLayoutWidget)
-        self.tableView.setObjectName(u"tableView")
-
-        self.horizontalLayout.addWidget(self.tableView)
-
-        self.verticalLayout_2 = QVBoxLayout()
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-
-        self.horizontalLayout.addLayout(self.verticalLayout_2)
-
-        self.pushButton_4 = QPushButton(self.centralwidget)
+        self.pushButton_4 = QPushButton(self.verticalLayoutWidget)
         self.pushButton_4.setObjectName(u"pushButton_4")
-        self.pushButton_4.setGeometry(QRect(710, 170, 89, 24))
+
+        self.verticalLayout.addWidget(self.pushButton_4)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 803, 22))
+        self.menubar.setGeometry(QRect(0, 0, 982, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -83,6 +158,17 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u5b66\u53f7(&B)", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\u59d3\u540d(&X)", None))
+        self.label_7.setText(QCoreApplication.translate("MainWindow", u"\u7535\u8bdd", None))
+        self.label_8.setText(QCoreApplication.translate("MainWindow", u"\u5bb6\u5ead\u4f4f\u5740", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u6027\u522b", None))
+        self.comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"\u7537", None))
+        self.comboBox.setItemText(1, QCoreApplication.translate("MainWindow", u"\u5973", None))
+
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"\u4e13\u4e1a", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"\u73ed\u7ea7", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"\u5b66\u9662", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u589e\u52a0", None))
         self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\u5220\u9664", None))
         self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"\u5bfc\u5165", None))
